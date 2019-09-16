c2545362b2270bfa2b0672301e2d2d1b22affb25
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-09-15 21:20:24 -0500

    spring batch parallel processing

---
+++ springbatch-splits/src/main/java/com/thanhhnguyen23/springbatch/SpringbatchSplitsApplication.java
@@ -0,0 +1,13 @@
+package com.thanhhnguyen23.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchSplitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchSplitsApplication.class, args);
+	}
+
+}
